1dfa763684d0aae2d037192072ede1c701732d98
Sokolinyy <kostyk.vasilii@gmail.com>
2023-02-25 01:03:20 +0100

    Some logic for find and show info about shows

--- index.js
+++ index.js
@@ -17,13 +17,19 @@ function searchShow(query) {
   // Loop through jsonData and if everything if ok, 
   // display name of TV show in <ul> of renderResult function
   .then((jsonData) => {
-    const resultOfSearch = jsonData.map(element => element.show.name)
-    const resultId = jsonData.map(element => element.show.id)
-    renderResults(resultOfSearch, query)
-    renderInfoAboutShow(resultId, query)
+    const results = jsonData.map(element => {
+      return {
+        name: element.show.name,
+        id: element.show.id,
+        image: element.show.image.medium
+      }
+    })
+
+    renderResults(results)
+/*     infoAboutShow(results) */
+/*     renderEpisodeList(resultId, query) */
     // If no errors, set error to empty string and not display it
     document.getElementById("errorMessage").innerHTML = "";
-    console.log(resultOfSearch);
   })
   .catch((error) => {
     // If have an error, display error, and clear renderResult
@@ -32,52 +38,71 @@ function searchShow(query) {
   })
 }
 
-function renderInfoAboutShow(resultId, query) {
-  console.log(resultId);
-  const urlEpisodeList = `https://api.tvmaze.com/shows/1/episodes`
-  fetch(urlEpisodeList)
-    .then((response) => {
-      return response.json()
-    })
-    .then((jsonData) => {
-      console.log(jsonData);
-
-      const link = document.getElementById("episodeQuantity")
-      link.addEventListener("click", () => {
-        displayEpisodeQuantity(jsonData.length, query);
-      })
-    })
-}
-
-function displayEpisodeQuantity(quantity, query) {
-  const message = `The TV show "${query}" has ${quantity} episodes.`;
-  alert(message);
-}
+/* function infoAboutShow(result) {
+  const urlEpisodeList = `https://api.tvmaze.com/shows/${result.id}/episodes`
+  
+} */
 
 // Function for rendering result of fetch, on the page
-function renderResults(results, query) {
-  const list = document.getElementById("resultList") // ! <ul>
-  // Always when user want to find, a new TV show, clear the resultList
+function renderResults(results) {
+  const list = document.getElementById("resultList")
   list.innerHTML = ""
 
-  const link = document.createElement("a")
-  link.href = `https://api.tvmaze.com/shows/${results}/episodes`
-
-  link.addEventListener('click', (event) => {
-    event.preventDefault(); // prevent the default action of following the link
-    const id = event.target.id
-    console.log(id);
-    renderInfoAboutShow()
-  });
-  // For each result, which grab from response, 
-  // create a new <li> element, inside the resultList <ul>
   results.forEach(result => {
+    const link = document.createElement("a")
+    link.href = `https://api.tvmaze.com/shows/${result.id}/episodes`
+
     const element = document.createElement("li")
-    element.innerText = result
-    link.appendChild(element) // Append the li element as a child of the a tag
+    element.innerText = result.name
+    link.appendChild(element)
     list.appendChild(link)
-  });
+
+    link.addEventListener("click", (event) => {
+      event.preventDefault()
+
+      fetch(link.href)
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        }
+        else {
+          throw new Error("HTTP Error")
+        }
+      })
+      .then(jsonData => {
+
+        const episodeList = document.createElement("ul")
+        for (let i = 0; i < jsonData.length; i++) {
+          const episode = jsonData[i]
+          const episodeItem = document.createElement("li")
+          episodeItem.innerText = `${episode.name} - Season ${episode.season} Episode ${episode.number} Episode Date ${episode.airdate}`
+          episodeList.appendChild(episodeItem)
+        }
+        link.appendChild(episodeList)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+
+    const name = document.createElement("h2")
+    name.innerText = result.name
+    link.appendChild(name)
+
+    const image = document.createElement("img")
+    image.src = result.image
+    link.appendChild(image)
+
+    list.appendChild(link)
+
+    console.log(result);
+
+    link.addEventListener("click", (event) => {
+      event.preventDefault()
+      })
+    })
+  })
 }
+
 
 let searchTimeoutToken = 0;
 
